ccbdc30aea4004cc8b08fa17a96fc92250982c8b
jaredbare <jab468@byu.edu>
2023-02-22 15:03:04 -0700

    Add project files.

---
+++ Project2_Group4/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project2_Group4.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
